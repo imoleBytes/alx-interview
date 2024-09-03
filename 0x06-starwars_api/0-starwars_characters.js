37f23a0d799ba4eef1d4801095f6c05e2bbaa4ff
imoleFolio <dremkay71@gmail.com>
2024-09-03 14:13:28 +0100

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,46 @@
 #!/usr/bin/node
+const request = require('request');
 
 const args = process.argv;
 const id = args[2];
 
 if (id) {
-  getFilms(id);
+  getFilmCharacters(id);
 }
 
-async function getFilms (id) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+// async function getFilms (id) {
+//   const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-  const resp = await fetch(url);
+//   const resp = await fetch(url);
 
-  const movie = await resp.json();
+//   const movie = await resp.json();
 
-  const characters = movie.characters;
+//   const characters = movie.characters;
 
+//   if (!characters) { return null; }
+//   for (const i of characters) {
+//     const re = await fetch(i);
+//     const character = await re.json();
+//     const name = character.name;
+//     console.log(name);
+//   }
+// }
+
+function getFilmCharacters (movieID) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+  request(url, (err, response, body) => {
+    if (err) {
+      return;
+    }
+    if (response.statusCode === 200) {
+      const characters = JSON.parse(body).characters;
+      printName(characters);
+    }
+  });
+}
+
+async function printName (characters) {
   if (!characters) { return null; }
   for (const i of characters) {
     const re = await fetch(i);
